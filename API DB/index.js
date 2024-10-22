cd599aac19c6d0012b0ee8d546f9d13a4aa3fd7b
Daniel Henrique Sell <kanee.sell@gmail.com>
2024-10-22 12:47:02 -0300

    api base de dados

---
+++ API DB/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const Firebird = require('node-firebird');
+
+//Config:
+const options = {
+    host:'127.0.0.1',
+    port: 3050,
+    database: 'C:\\Users\\kanee\\OneDrive\\Documentos\\GestorPro\\dados\\GP.FDB',
+    user: 'sysdba',
+    password: 'masterkey',
+    lowercase_keys: false,
+    role: null,
+    pageSize: 4096
+};
+
+const app = express()
+const port = 3000
+
+function connectToDatabase(query, callback) {
+    Firebird.attach(options, function(err, db) {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+
+      db.query(query, function(err, result) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, result);
+        }
+
+        // Fechar a conexão após a consulta
+        db.detach();
+      });
+    });
+  }
+
+app.get('/api/dados', (req, res) => {
+    const query = 'SELECT * FROM entidade'; // Substitua pela sua consulta SQL
+
+    connectToDatabase(query, (err, result) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else {
+        res.json(result);
+      }
+    });
+  });
+
+//iniciar servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+});
